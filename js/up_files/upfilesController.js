0b598ecb953f3e59fd52572f95f2c7826f007bdc
Jonathan Gonzalez <jonathangc.awt@gmail.com>
2019-03-10 20:21:55 -0500

    Hasta aqui el Storage funcionagit add -A con el cargador Realizaremos lo necesario para que la interfaz quede chunga y listo por esta parte

--- js/up_files/upfilesController.js
+++ js/up_files/upfilesController.js
@@ -1,32 +1,15 @@
 
-var formidable = require('formidable'),
-    http = require('http'),
-    util = require('util');
-
-/*
 //Obtencion de los elementos
-import firebase from 'firebase';
-import 'firebase/storage'; 
 
 var cargador = document.getElementById('bar_Cargador');
 var fileButton = document.getElementById('btn_Archivo');
 
 var iniciarApp=firebase.initializeApp(config);
-console.log(config);
-
-var storage = firebase.storage();
-//var storageRef = firebase.storage().ref().child("neo");
-/*
-var iniciarApp=firebase.initializeApp(config);
 
 // Get a reference to the storage service, which is used to create references in your storage bucket
 var storage = firebase.storage();
 
-// Create a storage reference from our storage service
-var storageRef = storage.ref();
-
-
-//var dbAlamcenamiento = iniciarApp.storage();
+var dbAlamcenamiento = iniciarApp.storage();
 
 
 //Vigilar la Seleccion de archivos
@@ -36,6 +19,22 @@ fileButton.addEventListener('change', function(e){
 	var file = e.target.files[0];
 	console.log(file);
 
+	//crear un storageRef
+	var storageRef = firebase.storage().ref('archivos/' + file.name);
+
+	//subir Archivo
+	var task = storageRef.put(file);
+
+	//Actualizar barra de progreso
+
+	task.on('state_changed',
+
+		function progress(snapshot){
+			var porcentage = (snapshot.bytesTransferred/snapshot.totalBytes) * 100
+			cargador.value = porcentage
+
+		}
+	)
+
 	
 }, false );
-*/
