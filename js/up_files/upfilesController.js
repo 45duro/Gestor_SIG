15341bffa3bd6f070fe91c074a573e4b1c9058e6
Jonathan Gonzalez <jonathangc.awt@gmail.com>
2019-03-12 10:34:02 -0500

    hasta aqui va mal, no es asi

--- js/up_files/upfilesController.js
+++ js/up_files/upfilesController.js
@@ -5,11 +5,13 @@ var cargador = document.getElementById('bar_Cargador');
 var fileButton = document.getElementById('btn_Archivo');
 
 
+
 // Get a reference to the storage service, which is used to create references in your storage bucket
 var storage = firebase.storage();
 var dbAlamcenamiento = iniciarApp.storage();
 
 function guardar(){
+	
 	console.log(email)
 	let archivo = document.getElementById('txt_nombreArchivo').value;
 	let codigoArchivo = document.getElementById('txt_codigoArchivo').value;
@@ -17,9 +19,6 @@ function guardar(){
 	let descripcion = document.getElementById('txt_descripcion').value;
 	let version = document.getElementById('txt_Version').value;
 	let ruta = "Documentos";
-
-
-
 	//Subir A la Base De Datos
 	bd.collection(ruta).add({
 		Nombre: archivo,
@@ -42,32 +41,36 @@ function guardar(){
 
 
 
+// function subirToFirebase(){
+// 	var imagenASubir = fileButton.files[0];
+	
+// }
 
 //Vigilar la Seleccion de archivos
-fileButton.addEventListener('change', function(e){
+// fileButton.addEventListener('change', function(e){
 	
-	//Obtener Archivo
-	var file = e.target.files[0];
-	console.log(file);
+// 	//Obtener Archivo
+// 	var file = e.target.files[0];
+// 	console.log(file);
 
-	//crear un storageRef
-	var storageRef = firebase.storage().ref('archivos/' + codigoArchivo);
+// 	//crear un storageRef
+// 	var storageRef = firebase.storage().ref('archivos/' + codigoArchivo);
 
-	//subir Archivo
-	var task = storageRef.put(file);
+// 	//subir Archivo
+// 	var task = storageRef.put(file);
 
-	//Actualizar barra de progreso
+// 	//Actualizar barra de progreso
 
-	task.on('state_changed',
+// 	task.on('state_changed',
 
-		function progress(snapshot){
-			var porcentage = (snapshot.bytesTransferred/snapshot.totalBytes) * 100
-			cargador.value = porcentage
+// 		function progress(snapshot){
+// 			var porcentage = (snapshot.bytesTransferred/snapshot.totalBytes) * 100
+// 			cargador.value = porcentage
 
-		}
-	)
+// 		}
+// 	)
 
 	
-}, false );
+// }, false );
 
 
