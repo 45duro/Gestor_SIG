41afb8263864118e57c93ee0bb25b08bf86535f5
Jonathan Gonzalez <jonathangc.awt@gmail.com>
2019-03-12 13:06:43 -0500

    pruebas nuevamente firebase iniciar app ya responde

--- js/general.js
+++ js/general.js
@@ -6,26 +6,26 @@ var photoURL = "";
 var isAnonymous = "";
 var providerData = "";
 
-// var iniciarApp=firebase.initializeApp(config);
+var iniciarApp=firebase.initializeApp(config);
 
-// firebase.auth().onAuthStateChanged((user)=>{
-// 	if (user) {
-// 		// User is signed in.
-// 		displayName = user.displayName;
-// 		email = user.email;
-// 		emailVerified = user.emailVerified;
-// 		photoURL = user.photoURL;
-// 		isAnonymous = user.isAnonymous;
-// 		uid = user.uid;
-// 		providerData = user.providerData;
+firebase.auth().onAuthStateChanged((user)=>{
+	if (user) {
+		// User is signed in.
+		displayName = user.displayName;
+		email = user.email;
+		emailVerified = user.emailVerified;
+		photoURL = user.photoURL;
+		isAnonymous = user.isAnonymous;
+		uid = user.uid;
+		providerData = user.providerData;
 		
-// 		// ...
-// 	} else {
-// 		console.log("Usuario no conocido")
-// 	}
-// });
+		// ...
+	} else {
+		console.log("Usuario no conocido")
+	}
+});
 
 
-console.log("hola");
+
 
 
