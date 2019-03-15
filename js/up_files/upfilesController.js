2f29c12badfad39ef6d06c83603bc5ade7c1ed92
Jonathan Gonzalez <jonathangc.awt@gmail.com>
2019-03-15 11:51:41 -0500

    Todas las modificaciones y toast funcionen correctamente

--- js/up_files/upfilesController.js
+++ js/up_files/upfilesController.js
@@ -11,7 +11,7 @@ toastr.options = {
   }
 
 
-
+//Funcion para determinar campos vacios
 function isVacio(elemento){
 
 	if (elemento==0){
@@ -24,18 +24,15 @@ function isVacio(elemento){
 }
 
 
-
-
-
-
+// Variables sistematicas
 var bd = firebase.firestore();
 var cargador = document.getElementById('bar_Cargador');
 var fichero = document.getElementById('fichero');
 var ruta;
 var file;
-$('#toas_Correcto').toast({ delay: 8000 });
-$('#toas_inCorrecto').toast({ delay: 8000 });
 
+
+//Fu8ncion para cargar el archivo desde el boton browse o examinar
 fichero.addEventListener('change', function(e){
 	
 	//Obtener Archivo
@@ -64,70 +61,69 @@ function guardar(){
 	//Determinar errores o vacios
 	if(isVacio(archivo) || isVacio(codigoArchivo) || isVacio(areaArchivo) || isVacio(archivo) || isVacio(version) || isVacio(fichero)){
 
-		console.log("Todos los campos deben estar diligenciados")
+		toastr.error(`Todos los campos deben estar diligenciados`, '¡Recuerda!');
 
 	}
 	else{
 		
-		console.log("Estoy en else");
-		// //Subir A la Base De Datos
-		// bd.collection(rutaBD).add({
-		// 	Nombre: archivo,
-		// 	Codigo: codigoArchivo,
-		// 	Ministerio: areaArchivo,
-		// 	Descripcion: descripcion,
-		// 	Version: version,
-		// 	Propiedad: displayName
-		// 	//Obtencion de los elementos
+		//Subir A la Base De Datos
+		bd.collection(rutaBD).add({
+			Nombre: archivo,
+			Codigo: codigoArchivo,
+			Ministerio: areaArchivo,
+			Descripcion: descripcion,
+			Version: version,
+			Propiedad: displayName
+			//Obtencion de los elementos
 			
-		// })
-		// .then(function(docRef){
+		})
+		.then(function(docRef){
 			
-		// 	// // console.log("Documento escrito con: ", docRef.id);
-		// 	//crear un storageRef
-		// 	var storageRef = firebase.storage().ref(ruta + codigoArchivo + " " + archivo);
+			// // console.log("Documento escrito con: ", docRef.id);
+			//crear un storageRef
+			var storageRef = firebase.storage().ref(ruta + codigoArchivo + " " + archivo);
 	
-		// 	//subir Archivo
-		// 	let task = storageRef.put(file);
+			//subir Archivo
+			let task = storageRef.put(file);
 	
-		// 	task.on('state_changed',
+			task.on('state_changed',
 		
-		// 		function progress(snapshot){
-		// 			let porcentage = (snapshot.bytesTransferred/snapshot.totalBytes) * 100
-		// 			cargador.value = porcentage;
+				function progress(snapshot){
+					let porcentage = (snapshot.bytesTransferred/snapshot.totalBytes) * 100
+					cargador.value = porcentage;
 	
-		// 			if(porcentage == 100){
+					if(porcentage == 100){
 						
-		// 				toastr.success(`El Documento ${archivo} esta en la nube correctamente`, '¡Excelente!');
+						toastr.success(`El Documento ${archivo} esta en la nube correctamente`, '¡Excelente!');
 	
-		// 				// document.getElementById('txt_nombreArchivo').value = '';
-		// 				// document.getElementById('txt_codigoArchivo').value = '';
-		// 				// document.getElementById('txt_areaArchivo').value = '';
-		// 				// document.getElementById('txt_descripcion').value = '';
-		// 				// document.getElementById('txt_Version').value = '';
-		// 				// document.getElementById('fichero').value = '';
-		// 				archivo.value = "";
-		// 				codigoArchivo.value = "";
-		// 				areaArchivo.value = "";
-		// 				descripcion.value = "";
-		// 				version.value = "";
-		// 				fichero.value = "";
-		// 				cargador.value = 0;
-		// 				console.log("Archivo subido correctamente");
+						// document.getElementById('txt_nombreArchivo').value = '';
+						// document.getElementById('txt_codigoArchivo').value = '';
+						// document.getElementById('txt_areaArchivo').value = '';
+						// document.getElementById('txt_descripcion').value = '';
+						// document.getElementById('txt_Version').value = '';
+						// document.getElementById('fichero').value = '';
+						archivo.value = "";
+						codigoArchivo.value = "";
+						areaArchivo.value = "";
+						descripcion.value = "";
+						version.value = "";
+						fichero.value = "";
+						cargador.value = 0;
+						console.log("Archivo subido correctamente");
 						
 	
-		// 			}
+					}
 	
 	
 		
-		// 		}
-		// 	)
-		// })
-		// .catch(err=>{
-		// 	toastr.error(`El Documento ${archivo} no se ha podido colocar en la nube`, '¡OH!');
-		// 	console.error("Error añadiendo el archivo: ", err.name)
+				}
+			)
+		})
+		.catch(err=>{
+			toastr.error(`El Documento ${archivo} no se ha podido colocar en la nube`, '¡OH!');
+			console.error("Error añadiendo el archivo: ", err.name)
 			
-		// })
+		})
 	}
 
 }
